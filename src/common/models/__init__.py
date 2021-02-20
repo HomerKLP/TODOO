65901708ab471dcc9cfa03211b78f15e78ae6f00
tshablin <shablin.t@chocofood.kz>
2021-02-20 17:01:23 +0600

    add base files

---
+++ src/common/models/__init__.py
@@ -0,0 +1,5 @@
+from typing import Tuple
+
+from .user import ExtendedUser
+
+__all__: Tuple = ('ExtendedUser',)
---
+++ src/common/models/user.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from common.models.entity import Entity, EntityUserManager
+
+
+class ExtendedUser(Entity, AbstractUser):
+
+    objects = EntityUserManager()
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
